140ea5f931f94b4bada3b1608098c78cc0454f88
milan.chauhan <milanchauhan555@gmail.com>
2023-10-20 20:11:33 +0100

    Add data quality table

--- QueryTweaks.php
+++ QueryTweaks.php
@@ -15,18 +15,17 @@ class QueryTweaks extends AbstractExternalModule {
         Get the number of open queries and append the number of open queries to Resolve Issues in the app panel
         */
 
-        $settings = $this->getProjectSettings();
-
         if (!isset($_GET['pid'])) return;
 
-        global $user_rights, $data_resolution_enabled;
+        $settings = $this->getProjectSettings();
+
+        global $user_rights, $data_resolution_enabled, $lang;
 
         if ($data_resolution_enabled == '2' && $user_rights['data_quality_resolution'] > 0) {
-            
 			// Get a count of unresolved issues
             $dq = new DataQuality();
-			$queryStatuses = $dq->countDataResIssues();
-			$numOpenIssues = $queryStatuses['OPEN'];
+            $queryStatuses = $dq->countDataResIssues();
+            $numOpenIssues = $queryStatuses['OPEN'];
             
             if ($settings['badges']) {
                 if ($numOpenIssues > 0) {
@@ -41,6 +40,36 @@ class QueryTweaks extends AbstractExternalModule {
                     </script>
                     <?php
                 }
+            }
+
+            switch (PAGE) {
+                case "index.php":
+                    // if (!settings['dq-table']) break;
+                    $dt_title = '<div style="padding: 0;"><i class="fas fa-comments"></i> ' . $lang['dataqueries_244'] . '</div>';
+                    $dt = '<div class="col-12 col-md-2 float-right">';
+                    $dt_col_widths_headers = array(
+                        array(140, '', 'left'),
+                        array(80,  '', 'center')
+                    );
+                    $dt_row_data = array();
+                    $order = array('OPEN', 'OPEN_UNRESPONDED', 'OPEN_RESPONDED', 'CLOSED', 'VERIFIED', 'DEVERIFIED');
+                    foreach ($order as $ord) {
+                        array_push($dt_row_data,
+                            array(
+                                ucwords(strtolower(str_replace('_', ' - ', $ord))),
+                                $queryStatuses[$ord]
+                            )
+                        );
+                    }
+                    $dt .= '<div>' .  renderGrid('dq-table', $dt_title, 220, 'auto', $dt_col_widths_headers, $dt_row_data, false) . '</div>';
+
+                    ?> <script type="text/javascript">
+                        $(document).ready(function() {
+                            $('#dq-table').appendTo($('div.float-right.text-left'));
+                        })
+                    </script>
+                    <?php
+                    break;
             }
         }
     }
